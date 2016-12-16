0e407c18fc0776850407bb5824035765d384d116
Richlee2016 <345642459@qq.com>
2016-12-16 17:44:54 +0800

    0

---
+++ src/components/multi/prefer/Prefer.vue
@@ -0,0 +1,114 @@
+<template>
+  <div class="pre-box">
+  	<div v-for="item in preAll" :class="item.addname" class="pre">
+  		<span></span>
+  		<p>{{item.description}}</p>
+  	</div>
+  	<!--<div>{{this.preData}}</div>-->
+  </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+export default {
+	props:{
+		preData:{
+			type:Array,
+			default:[]
+		}
+	},
+  computed:{
+  	preAll(){
+  		//如果有数据  增加一个转化类名
+  		if(this.preData.length >= 0){
+  			let ret = this.preData.map((o) => {
+  				o.addname = 'pre_' + o.type;
+  				return o;
+  			});
+  			return ret;
+  		};
+  	}
+  },
+  methods:{
+  	get(){
+  		this.preAll.forEach(function(o,i){
+  			console.log(o);
+  		});
+  	}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+.pre-box{
+	width: 100%;
+	height: auto;
+	float: left;
+	font-size: 0.16rem;
+	>div{
+		width: 100%;
+		height: 0.4rem;
+		line-height: 0.4rem;
+		margin-top: 0.1rem;
+		*{
+			float: left;
+		}
+		span{
+			width: 0.4rem;
+			height: 0.4rem;
+			&::before{
+				display: block;
+				width: 0.3rem;
+				height: 0.3rem;	
+				text-align: center;
+				line-height: 0.3rem;
+				margin-top: 0.05rem;
+				border-radius: 2px;		
+			}
+		}
+	}
+}
+.pre_0{
+	span{
+		&::before{
+			content: "减";
+			background:#f01414;
+		}
+	}
+}
+.pre_1{
+	span{
+		&::before{
+			content: "折";
+			background:#0f93cc;
+		}
+	}
+}
+.pre_2{
+	span{
+		&::before{
+			content: "特";
+			background:#00b43c;
+		}
+	}
+}
+.pre_3{
+	span{
+		&::before{
+			content: "票";
+			color: #8e007e;
+			border:#8e007e 1px solid;
+		}
+	}
+}
+.pre_4{
+	span{
+		&::before{
+			content: "减";
+			color: #00b43d;
+			border:#00b43d 1px solid;
+		}
+	}
+}
+</style>
---
+++ src/components/multi/star/Star.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="star-box" :class="starSize">
+  	<span v-for="item in starAllNum" :class="item" class="star"></span>
+  </div>
+</template>
+
+<script>
+import {mapGetters} from 'vuex'
+const STAR_ON = "star-on";
+const STAR_OFF = "star-off";
+const STAR_HALF = "star-half";
+export default {
+	props:{
+		starSize:{
+			type:Number,
+			default:1
+		},
+		starAll:{
+			type:Number,
+			default:5
+		},
+		starNum:{
+			type:Number,
+			default:0
+		}
+	},
+  data () {
+    return {
+    }
+  },
+  computed:{
+  	starAllNum(){
+  		let starArr = [],i;
+  		//半星计算
+			let starfloor = Math.floor(this.starNum*2)/2;
+			//是否有半星
+			let starmor = starfloor%1;
+			//添加全星
+			for( i=0; i<starfloor; i++){
+				starArr.push(STAR_ON);
+			};
+			//添加半星
+			if(starmor){
+				i++;
+				starArr.push(STAR_HALF);
+			};
+			//添加空星
+			while(i != this.starAll ){
+				i++;
+				starArr.push(STAR_OFF);
+			};
+			return starArr;
+  	}
+  },
+  methods:{
+  	
+  },
+  created() {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+.star-box{
+	overflow: hidden;
+	height: 0.4rem;
+	span{
+		float: left;
+		margin: 0rem 0.1rem;
+		width: 0.4rem;
+		height: 0.4rem;
+	}
+	.star-on{
+		background: yellowgreen;
+	}
+	.star-half{
+		background: cornflowerblue;
+	}
+	.star-off{
+		background: royalblue;
+	}
+}
+</style>
